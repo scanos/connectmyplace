68d07e6932305f3134185514d84ff0009a11f731
root <root@scanos.hopto.org>
2021-03-23 10:38:04 +0000

    Script added

---
+++ wind chart/wind_connectmyplace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+wind=$( echo $(sudo curl "https://api.openweathermap.org/data/2.5/weather?lat=54.38&lon=-5.54&APPID=YOUR_API_ID" | jq '.wind.speed'))
+wind=$(echo "scale=0; $wind * 2.23694" | bc )
+wind=${wind%.*} #return substring before decimal point
+
+mydate=$(date "+%d-%m-%Y %H:%M")
+
+echo $mydate "$wind" "Wind" "Speed" "mph"
+
+echo "['$mydate', $wind]," >> wind.log
+cat windheader.html wind.log windfooter.html > windchartline.html
+cp windchartline.html /var/www/html/windchartline.html
+
+#echo "$mydate, $temp_loft, $temp_office, $wind, $outside_temperature"
+
+
