9d2d065837e877bd6f35dc6aff83ce447aa57af7
Seamus Kane <seamuskane@aim.com>
2021-04-03 21:09:03 +0100

    Update dfplayer_attiny85_dsb1820.ino

--- dfplayer/dfplayer_attiny85_dsb1820.ino
+++ dfplayer/dfplayer_attiny85_dsb1820.ino
@@ -3,21 +3,37 @@ this works as at 4th April 2021
 powered by CNT003 g -> AT g, 3.3V to AT  vcc, RX to AT tx (pin 3)
 */
 
+#include "Arduino.h"
 #include <SoftwareSerial.h>
+#include "DFRobotDFPlayerMini.h"
 #include <OneWire.h>
+SoftwareSerial mySoftwareSerial(RX, TX); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+//void printDetail(uint8_t type, int value);
 #define TX    4   // *** D4, Pin 3
 #define ONEWIRE_BUSS 7 // *** D2 beside VCC
 //1 mhz timer
 int temp_prev;
 int temp_counter=0;
-SoftwareSerial Serial(0, TX);
+
 OneWire TemperatureSensor(ONEWIRE_BUSS);
 extern uint8_t BigNumbers[];
 
 void setup(void) {
 
-  Serial.begin(9600);
-  Serial.println("Initializing...");
+  //Serial.begin(9600);
+  //Serial.println("Initializing...");
+   mySoftwareSerial.begin(9600);
+
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+
+    while(true);
+  }
+ 
+
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+  //myDFPlayer.play(1);  //Play the first mp3
 
 
 }
@@ -46,7 +62,7 @@ raw = (data[1] << 8) | data[0];
 t = (float)raw / 16.0;
 ti = (int) t;
 
-Serial.print(" temp");Serial.println(ti);
+//Serial.print(" temp");Serial.println(ti);
 delay (5000);
 /* 30 seconds */
 
