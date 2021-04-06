d96251c6cb50b8ab1b0c0958efeea38d47b7cc0c
Seamus Kane <seamuskane@aim.com>
2021-04-06 19:26:53 +0100

    Update dfplayer_attiny85_dsb1820.ino

--- dfplayer/dfplayer_attiny85_dsb1820.ino
+++ dfplayer/dfplayer_attiny85_dsb1820.ino
@@ -1,29 +1,47 @@
-/* for evaluation you can power it from a CNT-003A (CP210X on device manager port)
-this works as at 4th April 2021 
-powered by CNT003 g -> AT g, 3.3V to AT  vcc, RX to AT tx (pin 3)
+/*DFPlayer - A Mini MP3 Player For Arduino
+ <https://www.dfrobot.com/index.php?route=product/product&product_id=1121>
+ ***************************************************
+ This example shows the basic function of library for DFPlayer.
+ Created 2016-12-07
+ By [Angelo qiao](Angelo.qiao@dfrobot.com)
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+ 1.Connection and Diagram can be found here
+ <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
+ 2.This code is tested on Arduino Uno, Leonardo, Mega boards.
+ ***************************************************
+ConnectMyPlace and to get it to work on ATTINY, first start the DF player and then power on the ATTINY85
+On the dfplayer pin out is vcc rx tx
+On the Attiny85 it is      rx  tx gnd
+tx <> rx
+rx <> tx
+//ATTINY85 clock 1 mhz 
 */
 
 #include "Arduino.h"
 #include <SoftwareSerial.h>
 #include "DFRobotDFPlayerMini.h"
 #include <OneWire.h>
-SoftwareSerial mySoftwareSerial(RX, TX); // RX, TX
-DFRobotDFPlayerMini myDFPlayer;
-//void printDetail(uint8_t type, int value);
-#define TX    4   // *** D4, Pin 3
 #define ONEWIRE_BUSS 7 // *** D2 beside VCC
-//1 mhz timer
-int temp_prev;
-int temp_counter=0;
 
 OneWire TemperatureSensor(ONEWIRE_BUSS);
 extern uint8_t BigNumbers[];
+#define RX    3   // *** D3, Pin 2
+#define TX    4   // *** D4, Pin 3
+SoftwareSerial mySoftwareSerial(RX, TX); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
 
-void setup(void) {
+int tc = 0; // temperature counter - to allow for temperature sensor equiibrating
+int maxvalue = 55;
 
-  //Serial.begin(9600);
-  //Serial.println("Initializing...");
-   mySoftwareSerial.begin(9600);
+void setup()
+{
+  delay(3000);
+  mySoftwareSerial.begin(9600);
 
 
   if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
@@ -31,13 +49,8 @@ void setup(void) {
     while(true);
   }
  
-
   myDFPlayer.volume(30);  //Set volume value. From 0 to 30
-  //myDFPlayer.play(1);  //Play the first mp3
-
-
-}
-
+  }
 
 void loop()
 {
@@ -61,17 +74,23 @@ data[i] = TemperatureSensor.read();
 raw = (data[1] << 8) | data[0];
 t = (float)raw / 16.0;
 ti = (int) t;
-    myDFPlayer.play(ti);  //Play next mp3 every 3 second.
-  }
+delay(3000);
+if (ti < maxvalue && tc > 1){
+    ti = ti + 30;
+    myDFPlayer.play(ti);
+    }
+    else
+    {
+    //myDFPlayer.play(92); //sensor stabilize alert
+    }
+  
 
   if (myDFPlayer.available()) {
-    //printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
-  myDFPlayer.readType(), myDFPlayer.read();  
+    myDFPlayer.readType(), myDFPlayer.read();  
+
    }
-//Serial.print(" temp");Serial.println(ti);
-delay (5000);
-/* 30 seconds */
 
+   delay(5000);
+   tc = tc + 1;
+     
 }
-
-
